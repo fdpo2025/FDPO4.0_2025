3225132908027f062b9fcedca0a742d52fac5c3f
Daniel Silva <daniel.f.q.d.s@gmail.com>
2026-03-20 14:41:06 +0000

    aliviar condicao approaching + debug error_dist

--- navigation_stack/navigation_controller/src/navigation_controller_node.cpp
+++ navigation_stack/navigation_controller/src/navigation_controller_node.cpp
@@ -742,8 +742,8 @@ void NavigationController::followLine() {
     // Vai para Approaching quando tiver percorrido approaching_line_progress da linha E
     // (o ponto final for uma warehouse OU se estiver saindo de uma warehouse (backwards=true))
     if (followLineFsm.state == navigation::followLineStates::Follow_Line) {
-        if (error_dist < param.dist_da && (line.pf.is_warehouse || isBackwards())) {
-            ROS_WARN("approaching state condition met");
+        if (error_dist < param.dist_da /*&& (line.pf.is_warehouse || isBackwards())*/) {
+            ROS_WARN("approaching state condition met - error_dist=%.3f < dist_da=%.3f", error_dist, param.dist_da);
             followLineFsm.new_state = navigation::followLineStates::Approaching;
         }
     }
