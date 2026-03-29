24df388e136d525d9293fc71c7d66617ceb7c5a2
kanestrene <pedruxande@gmail.com>
2026-03-29 16:19:18 +0100

    retries mandar caminho para 10000

--- drivers_stack/pi_pico_driver/src/pico_driver_node.cpp
+++ drivers_stack/pi_pico_driver/src/pico_driver_node.cpp
@@ -325,7 +325,7 @@ void PiPicoDriver::cpSendCallBack(const std_msgs::UInt32::ConstPtr& msg) {
 
 void PiPicoDriver::pathSendCallBack(const std_msgs::Int32MultiArray::ConstPtr& msg) {
   path_to_send_ = msg->data;
-  path_send_retries_ = 1000;
+  path_send_retries_ = 10000;
 }
 
 void PiPicoDriver::pubExtraMsgs() {
